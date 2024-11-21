01a245897b02a5e0c0cef6c12378aaed9c8149ba
Joicemotamoreira <joicemotamoreira@live.com>
2024-11-21 18:31:47 +0000

    Added logo to the project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import "./App.css";
+import logo from "./logo/logo.png";
 import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">Dictionary App</header>
+        <header className="App-header">
+          <a
+            href="https://github.com/Joicemotamoreira/dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={logo} alt="image logo" className="logo img-fluid"></img>
+          </a>
+        </header>
         <main>
           <Dictionary defaultKeyword="beach" />
         </main>
 
-        <footer className="App-footer">Coded by Joice Mota</footer>
+        <footer className="App-footer">
+          Coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/joicemotamoreira/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Joice
+          </a>{" "}
+          with 🤎!
+        </footer>
       </div>
     </div>
   );
