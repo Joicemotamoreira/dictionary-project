0d59e32caea578690b53d340398c9e132fb86e54
Joicemotamoreira <joicemotamoreira@live.com>
2024-11-21 09:43:44 +0000

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary App</header>
+      <div className="container">
+        <header className="App-header">Dictionary App</header>
+        <main>
+          <Dictionary />
+        </main>
+
+        <footer className="App-footer">Coded by Joice Mota</footer>
+      </div>
     </div>
   );
 }
